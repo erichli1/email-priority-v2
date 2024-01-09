df692037c1bcdbdea7ad7247d5782dd6edcbd0e0
erichli1 <ehli@college.harvard.edu>
2024-01-09 13:11:38 -0500

    Add logic to start/stop watching inbox

--- app/page.tsx
+++ app/page.tsx
@@ -4,15 +4,12 @@ import { Button } from "@/components/ui/button";
 import {
   Authenticated,
   Unauthenticated,
-  useMutation,
+  useAction,
   useQuery,
 } from "convex/react";
 import { api } from "@/convex/_generated/api";
-import { Code } from "@/components/typography/code";
-import { Link } from "@/components/typography/link";
 import { SignInButton, SignUpButton, UserButton } from "@clerk/clerk-react";
 import { StickyHeader } from "@/components/layout/sticky-header";
-import { Skeleton } from "@/components/ui/skeleton";
 
 export default function Home() {
   return (
@@ -57,62 +54,30 @@ function SignInAndSignUpButtons() {
 }
 
 function SignedInContent() {
-  const { viewer, numbers } =
+  const { viewer } =
     useQuery(api.myFunctions.listNumbers, {
       count: 10,
     }) ?? {};
-  const addNumber = useMutation(api.myFunctions.addNumber);
-
-  if (viewer === undefined || numbers === undefined) {
-    return (
-      <>
-        <Skeleton className="h-5 w-full" />
-        <Skeleton className="h-5 w-full" />
-        <Skeleton className="h-5 w-full" />
-      </>
-    );
-  }
+  const startWatching = useAction(api.nodeActions.startWatching);
+  const stopWatching = useAction(api.nodeActions.stopWatching);
 
   return (
     <>
       <p>Welcome {viewer ?? "N/A"}!</p>
-      <p>
-        Click the button below and open this page in another window - this data
-        is persisted in the Convex cloud database!
-      </p>
-      <p>
-        <Button
-          onClick={() => {
-            void addNumber({ value: Math.floor(Math.random() * 10) });
-          }}
-        >
-          Add a random number
-        </Button>
-      </p>
-      <p>
-        Numbers:{" "}
-        {numbers?.length === 0
-          ? "Click the button!"
-          : numbers?.join(", ") ?? "..."}
-      </p>
-      <p>
-        Edit <Code>convex/myFunctions.ts</Code> to change your backend
-      </p>
-      <p>
-        Edit <Code>app/(fullstack)/page.tsx</Code> to change your frontend
-      </p>
-      <p>
-        Check out{" "}
-        <Link target="_blank" href="https://docs.convex.dev/home">
-          Convex docs
-        </Link>
-      </p>
-      <p>
-        To build a full page layout copy one of the included{" "}
-        <Link target="_blank" href="/layouts">
-          layouts
-        </Link>
-      </p>
+      <Button
+        onClick={() => {
+          startWatching().catch(console.error);
+        }}
+      >
+        Start watching
+      </Button>
+      <Button
+        onClick={() => {
+          stopWatching().catch(console.error);
+        }}
+      >
+        Stop watching
+      </Button>
     </>
   );
 }
--- convex/nodeActions.ts
+++ convex/nodeActions.ts
@@ -2,6 +2,9 @@
 
 import { action } from "./_generated/server";
 import { v } from "convex/values";
+import clerkClient from "@clerk/clerk-sdk-node";
+import { gmail } from "@googleapis/gmail";
+import { OAuth2Client } from "google-auth-library";
 
 export const decodeBodyMessageData = action({
   args: {
@@ -9,3 +12,57 @@ export const decodeBodyMessageData = action({
   },
   handler: async (_, { data }) => Buffer.from(data, "base64url").toString(),
 });
+
+export const startWatching = action({
+  handler: async (ctx) => {
+    const user = await ctx.auth.getUserIdentity();
+    if (user) {
+      const client = await getGmailClient({ clerkUserId: user.subject });
+
+      const response = await client.users.watch({
+        userId: "me",
+        requestBody: {
+          topicName: "projects/email-priority-classifier/topics/gmail",
+          labelIds: ["INBOX"],
+        },
+      });
+      console.log(`watch response status ${response.status}`);
+    }
+  },
+});
+
+export const stopWatching = action({
+  handler: async (ctx) => {
+    const user = await ctx.auth.getUserIdentity();
+    if (user) {
+      const client = await getGmailClient({ clerkUserId: user.subject });
+      const response = await client.users.stop({
+        userId: "me",
+      });
+      console.log(`stop response status ${response.status}`);
+    }
+  },
+});
+
+const getGmailClient = async ({ clerkUserId }: { clerkUserId: string }) => {
+  const [accessToken] = await clerkClient.users.getUserOauthAccessToken(
+    clerkUserId,
+    "oauth_google"
+  );
+
+  const oauth2 = new OAuth2Client({
+    clientId: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  });
+
+  oauth2.setCredentials({
+    access_token: accessToken.token,
+  });
+
+  const client = gmail({
+    version: "v1",
+    auth: oauth2,
+  });
+
+  return client;
+};
